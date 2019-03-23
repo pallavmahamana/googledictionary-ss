9b450db3bda8202ac4b6c0cb6e392b2dcf6c4271
Pallav Mahamana <spsec.pallav@gmail.com>
2019-03-23 14:38:35 +0530

    changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/api/list',function(req, res){
 			console.log(err);
 		else {
 			res.setHeader('Content-Type', 'application/json');
-			res.end(result);
+			res.send(result);
 		}
 	})
 });
